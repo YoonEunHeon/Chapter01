e53c419815580c43477635e9972d2a43be221da7
sch <Sch841@@naver.com>
2017-01-03 15:44:10 +0900

    switch

--- src/chapter01/SwitchTest.java
+++ src/chapter01/SwitchTest.java
@@ -6,40 +6,35 @@ public class SwitchTest {
 		// TODO Auto-generated method stub
 	 int month = 10;
 	 int days = 0;
-	 
-	 if(month == 1 || 
-	    month ==3  ||
-	    month == 5 || 
-	    month == 7 || 
-	    month == 8 || 
-	    month ==10 || 
-	    month ==12){
-		 days = 31;		 
-	 }else if ( month ==2){
-		 days = 28;
-	 }else if (month == 3) {
-		 days = 31;
-	 }else if (month == 4) {
-		 days = 30;
-	 }else if (month == 5) {
-		 days = 31;
-	 }else if (month == 6) {
-		 days = 30;
-	 }else if (month == 7) {
-		 days = 31;
-	 }else if (month == 8) {
-		 days = 31;
-	 }else if (month == 9) {
-		 days = 30;
-	 }else if (month == 10) {
-		 days = 31;
-	 }else if (month == 11) {
-		 days = 30;
-	 }else if (month == 12) {
-		 days = 31;
-	 }else {
-		 days = 0;
+	  switch(month){
+	  case 1 : 
+	  case 3 : 
+	  case 5 : 
+	  case 7 : 
+	  case 8 : 
+	  case 10 :
+	  case 12 : 
+	  {
+		  days = 31;
+		  break;
 	 }
-	}
+	  case 2 : {
+		  days = 29;
+		  break;
+	  }
 
+	  default : {
+		  days = 30;
+		  break; 
+	  }
+	  
+	   
+	  }
+	  System.out.println("이번달은 " + days +"일 까지입니다.");
+    
+	}
 }
+
+
+
+
